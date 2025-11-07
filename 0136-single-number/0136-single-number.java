285f140fe1ccb489addaaa9e10537318a675cb09
Subir Chanda <115805553+Subir-Chanda@users.noreply.github.com>
2025-11-08 00:51:12 +0530

    Time: 1 ms (99.89%), Space: 47 MB (6.45%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,20 +1,10 @@
-import java.util.*;
+
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        int sum=0;
-        int mul=0;
-        for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
-                set.remove(nums[i]);
-            }
-            else{
-                set.add(nums[i]);
-                mul+=2*nums[i];
-
-            }
-            sum+=nums[i];
+        int k=nums[0];
+        for(int i=1;i<nums.length;i++){
+            k=k^nums[i];
         }
-        return mul-sum;
+        return k;
     }
 }
